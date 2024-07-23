20727e815ffae06049f37dec2422f1f4ac2c1f9e
vishalipar <vishalipar04@gmail.com>
2024-07-23 23:43:45 +0530

    Developed conversation, browse, inbox, dashboard and add new items.

--- puddle/urls.py
+++ puddle/urls.py
@@ -24,5 +24,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('core.urls')),
     path('dashboard/', include('dashboard.urls')),
+    path('inbox/', include('conversation.urls')),
     path('items/', include('item.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
